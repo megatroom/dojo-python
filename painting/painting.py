10c31652988a0f7478222e05e408251d4a85a5d6
Bruno Nardini <bruno.nardini@resultadosdigitais.com.br>
2020-10-30 14:13:37 -0300

    Add painting exercise

---
+++ painting/painting.py
@@ -0,0 +1,11 @@
+
+width = float(input("Largura da parede: "))
+height = float(input("Altura da parede: "))
+
+area = width * height
+
+print(f"A parede mede {width}x{height} com a área de {area}m²")
+
+ink_volume = area / 2
+
+print(f"Você irá precisar de {ink_volume}L de tinta")

0bd94f87bf4596c8c1fa02a4258e4d079677c84d
Bruno Nardini <bruno.nardini@resultadosdigitais.com.br>
2020-11-09 17:50:49 -0300

    Add roman numbers exercise

---
+++ romannumbers/roman_numbers.py
@@ -0,0 +1,7 @@
+
+roman_numbers = ["", "I", "II", "III", "IV",
+                 "V", "VI", "VII", "VIII", "IX", "X"]
+
+number = int(input("Digite o número: "))
+
+print(f"Equivale ao número romano {roman_numbers[number]}")
